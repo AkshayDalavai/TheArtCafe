147761fd6dc6f65cf888812575ef99cbead71597
AkshayDalavai <dalavaigame@gmail.com>
2020-11-03 12:03:09 -0500

    Adding spring boot boiler plate code

---
+++ theartcafebackend/src/main/java/com/backend/theartcafebackend/TheArtCafeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.theartcafebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheArtCafeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheArtCafeBackendApplication.class, args);
+	}
+
+}
---
+++ theartcafebackend/src/test/java/com/backend/theartcafebackend/TheArtCafeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.theartcafebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheArtCafeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
